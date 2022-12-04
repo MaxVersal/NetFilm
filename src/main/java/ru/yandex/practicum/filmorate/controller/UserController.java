55e307a7ad79d6e2d5394665078c1529c88400af
MaxVersal <m.stowell@mail.ru>
2022-12-04 19:11:02 +0300

    files refactoring

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private static final Map<Integer, User> users = new HashMap<>();
+    private static int newId = 1;
+
+    @GetMapping
+    public List< User> getUsers() {
+        List<User> us = new ArrayList<>();
+        for (User user : users.values()) {
+            us.add(user);
+        }
+        return us;
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) throws ValidationException {
+        if (user.getName() == null) {
+            user.setName(user.getLogin());
+        }
+        if (checkUser(user)) {
+            user.setId(newId++);
+            log.debug("Добавлен пользователь: {}", user);
+            users.put(user.getId(), user);
+        } else {
+            throw new ValidationException("Проверьте введенные данные!");
+        }
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) throws ValidationException {
+        if (checkUser(user)){
+            if (users.containsKey(user.getId())) {
+                log.debug("Обновлен фильм: {}", user);
+                users.put(user.getId(), user);
+            } else {
+                throw new ValidationException("Проверьте правильность введенных данных");
+            }
+        } else {
+            throw new ValidationException("Проверьте введенные данные!");
+        }
+        return user;
+    }
+
+    public boolean checkUser(User user) {
+        if (user.getEmail().isEmpty() || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            log.debug("Неверное указан email пользователя");
+            return false;
+        } else if (user.getLogin().contains(" ") || user.getLogin().isEmpty() || user.getLogin().isEmpty()) {
+            log.debug("Неверный логин");
+            return false;
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.debug("Неверно указана дата рождения");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+}
